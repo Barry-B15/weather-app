16244aaeff7b918ced741ce675fe9a5ee208a28d
Barry <Barry-B15@users.noreply.github.com>
2020-04-03 15:14:15 +0900

    button click listener

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,48 @@
+//8. re-arrange
+//8.1 def the lat lon
+let lat, lon;
+
+//def the btn and add click listener
+const button = document.getElementById('submit');
+button.addEventListener('click', async event => {
+    const car = document.getElementById('cars').value;
+
+    //8.2 move this data here from the geolocation
+    const data = {
+        lat,
+        lon,
+        car // add the new element
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('api', options); //8.3 use 'api' instead of '/api' otherwise the lat lon wont be saved to db. Lost time tracing this.
+    const json = await response.json();
+    console.log(json);
+});
+
+
 //take data (geolocation) from client
 if ('geolocation' in navigator) {
     console.log('geolocation available');
 
     //get current position
-    navigator.geolocation.getCurrentPosition(async position => { //6.0 make this an async func
+    navigator.geolocation.getCurrentPosition(async position => {
         //def lat lon and get the coords
+        /* 8.4 remove the const, use lat lon we already defined globally
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
+         */
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
         document.getElementById('latitude').textContent = lat;
         document.getElementById('longitude').textContent = lon;
         console.log(position); //console log the pos
-
-        //5.1 (see server side) fetch/send user data in json format
-        const data = {
-            lat,
-            lon
-        }
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        };
-        //6.1 Make fetch await()
-        /* fetch('/api', options).then(response => {  //5.3 add then()
-            console.log(response);
-        }); */
-        const response = await fetch('/api', options);
-        const json = await response.json();
-        console.log(json);
     });
-
-    // 7.6 insert user data
-    //database.insert(data);
-    //console.log(database);
 } else {
     console.log('geolocation not available'); // if geolocation is not available
 }
